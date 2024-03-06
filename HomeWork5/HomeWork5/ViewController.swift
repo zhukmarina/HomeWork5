001a2398b0cbbee04c1c0e365bc49f24ab80b79b
Marina Zhukova <marinazhukova28@gmail.com>
2024-03-06 16:41:40 +0200

    add empty project

---
+++ HomeWork5/HomeWork5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork5
+//
+//  Created by Marina Zhukova on 06.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
