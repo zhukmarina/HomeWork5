b9c10a4177a8a7aeeb92b8a2fcdc6a39d34c2078
Marina Zhukova <marinazhukova28@gmail.com>
2024-03-07 18:58:22 +0200

    task done

--- HomeWork5/HomeWork5/ViewController.swift
+++ HomeWork5/HomeWork5/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var signUpButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
